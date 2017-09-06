e1e459a3738425ec4c0eebf80a8a5bb5cbd44d62
tinaleios <tianleios@163.com>
2017-09-06 15:25:49 +0800

    first commit

---
+++ CDCommon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CDCommon
+//
+//  Created by  tianlei on 2017/9/6.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CDCommon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDCommon
+//
+//  Created by  tianlei on 2017/9/6.
+//  Copyright © 2017年  tianlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
